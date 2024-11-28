a273750e2050cc6d20cb6ba7c4a3931573c5ab5b
kaurovaae <kaurova.a.e@gmail.com>
2024-11-28 20:49:04 +0300

    fix: order

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -9,10 +9,10 @@ import {
 
 const router = Router();
 
-router.get('/', getCards); // GET /cards — возвращает все карточки
-router.post('/', createCard); // POST /cards — создаёт карточку
 router.put('/:cardId/likes', likeCard); // PUT /cards/:cardId/likes — поставить лайк карточке
 router.delete('/:cardId/likes', dislikeCard); // DELETE /cards/:cardId/likes — убрать лайк с карточки
 router.delete('/:cardId', deleteCard); // DELETE /cards/:cardId — удаляет карточку по идентификатору
+router.get('/', getCards); // GET /cards — возвращает все карточки
+router.post('/', createCard); // POST /cards — создаёт карточку
 
 export default router;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -9,10 +9,10 @@ import {
 
 const router = Router();
 
-router.get('/', getUsers); // GET /users — возвращает всех пользователей
-router.get('/:userId', getUser); // GET /users/:userId - возвращает пользователя по _id
-router.post('/', createUser); // POST /users — создаёт пользователя
 router.patch('/me/avatar', updateUserAvatar); // PATCH /users/me/avatar — обновляет аватар
 router.patch('/me', updateUser); // PATCH /users/me — обновляет профиль
+router.get('/:userId', getUser); // GET /users/:userId - возвращает пользователя по _id
+router.get('/', getUsers); // GET /users — возвращает всех пользователей
+router.post('/', createUser); // POST /users — создаёт пользователя
 
 export default router;
