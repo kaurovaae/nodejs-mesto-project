aee7586df37e8f750d1c1579ec44e90c0e94ecf2
kaurovaae <kaurova.a.e@gmail.com>
2024-12-01 13:49:36 +0300

    fix: use auth middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import authRouter from './auth';
 import usersRouter from './users';
 import cardsRouter from './cards';
+import auth from '../middlewares/auth';
 
 const router = Router();
 
 router.use('/', authRouter);
+router.use(auth);
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
