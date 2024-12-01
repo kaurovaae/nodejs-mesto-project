e1450c5d20bbadf16f445ea01292265bbbc1362b
kaurovaae <kaurova.a.e@gmail.com>
2024-12-01 21:30:55 +0300

    fix: support not found error

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import authRouter from './auth';
 import usersRouter from './users';
 import cardsRouter from './cards';
 import auth from '../middlewares/auth';
+import { Request, Response, NextFunction } from '../Model/Express';
+import NotFoundError from '../errors/not-found-err';
 
 const router = Router();
 
@@ -10,5 +12,9 @@ router.use('/', authRouter);
 router.use(auth);
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+
+router.use('*', (_req: Request, _res: Response, next: NextFunction) => {
+  return next(new NotFoundError('Запрашиваемый ресурс не найден'));
+});
 
 export default router;
