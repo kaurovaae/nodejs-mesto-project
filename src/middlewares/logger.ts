f7115c4b0fd9ca5b3686f8b63167f9246e0f826b
kaurovaae <kaurova.a.e@gmail.com>
2024-12-01 20:00:57 +0300

    fix: remove debug

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -15,9 +15,9 @@ export const requestLogger = expressWinston.logger({
 
 export const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.Console({
-      format: winston.format.prettyPrint(),
-    }),
+    // new winston.transports.Console({
+    //   format: winston.format.prettyPrint(),
+    // }),
     new winston.transports.File({
       filename: 'logs/error.log',
     }),
