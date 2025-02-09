fa5cdcc6a88a4b562f73cbce24be547eb03aa14e
kaurovaae <kaurova.a.e@gmail.com>
2025-02-09 15:08:41 +0300

    fix: ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main',
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/main',
+  JWT_SECRET,
 } = process.env;
 
 module.exports = {
   apps: [{
     name: 'mesto-backend',
     script: './dist/app.js',
+    env_production: {
+      NODE_ENV: 'production',
+      JWT_SECRET,
+    },
   }],
 
   // Настройка деплоя
@@ -18,7 +26,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/kaurovaae/nodejs-mesto-project.git',
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm i && npm run build',
     },
   },
