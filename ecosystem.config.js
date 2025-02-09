1652253dcef71a41427fc379ee451aeee4d072d7
kaurovaae <kaurova.a.e@gmail.com>
2025-02-09 15:21:11 +0300

    fix: pm2 settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -26,7 +26,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/kaurovaae/nodejs-mesto-project.git',
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm i && npm run build && pm2 startOrRestart ecosystem.config.js',
     },
   },
